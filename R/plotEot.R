92804364408afdcb05653320a4323b9b798eb7b3
tappelhans <tim.appelhans@gmail.com>
2013-08-07 16:28:31 +0200

    UPDATE: include add.map for plotting function and update depends list

--- R/plotEot.R
+++ R/plotEot.R
@@ -2,6 +2,7 @@ plotEot <- function(eot.outlist,
                     eot = 1,
                     pred.prm = "rsq.predictor",
                     resp.prm = "rsq.response",
+                    add.map = TRUE,
                     clr = colorRampPalette(
                       rev(brewer.pal(9, "Spectral")))(1000))
 {
@@ -12,18 +13,34 @@ plotEot <- function(eot.outlist,
   eot.location.p <- xyplot(xy[1, 2] ~ xy[1, 1], cex = 2,
                            pch = 21, fill = "grey80", col = "black")
   
-  pred.p <- spplot(eot.outlist[[1]][[pred.prm]][[eot]],
+  if (isTRUE(add.map)) {
+    mm <- map("world", plot = F, fill = T, col = "grey70")
+  }
+  
+  pred.p <- spplot(eot.outlist[[1]][[pred.prm]][[eot]], mm = mm,
                    colorkey = list(space = "top",
                                    width = 0.7, height = 0.8), 
                    main = pred.prm, 
-                   col.regions = clr) +
+                   col.regions = clr, panel = function(..., mm) {
+                     panel.levelplot(...)
+                     if (isTRUE(add.map)) {
+                     panel.polygon(mm$x, mm$y, lwd = 0.5, 
+                                   border = "grey20")
+                     }
+                     }) +
     as.layer(eot.location.p)
   
-  resp.p <- spplot(eot.outlist[[1]][[resp.prm]][[eot]],
+  resp.p <- spplot(eot.outlist[[1]][[resp.prm]][[eot]], mm = mm,
                    colorkey = list(space = "top",
                                    width = 0.7, height = 0.8), 
                    main = resp.prm, 
-                   col.regions = clr) +
+                   col.regions = clr, panel = function(..., mm) {
+                     panel.levelplot(...)
+                     if (isTRUE(add.map)) {
+                       panel.polygon(mm$x, mm$y, lwd = 0.5, 
+                                     border = "grey20")
+                     }
+                   }) +
     as.layer(eot.location.p)
   
   ### clear plot area
