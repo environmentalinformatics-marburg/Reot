e42cee535770d906cffbe806b33e4c427adefeba
tim-salabim <tim.appelhans@gmail.com>
2013-08-27 20:56:28 +0200

    UPDATE: plotLocations now properly plots points (on predictor extent) and legend

--- R/plotLocations.R
+++ R/plotLocations.R
@@ -22,40 +22,80 @@ plotLocations <- function(eot.obj) {
                    }) 
   
   clrs.hcl <- function(n) {
-    hcl(h = seq(230, 0, length.out = n), 
-        c = 60, l = 50, fixup = TRUE)
+    hcl(h = seq(270, 0, length.out = n), 
+        c = 70, l = 50, fixup = TRUE)
   }
   
+  clrs <- clrs.hcl(length(eot.obj[[1]]))
+  
   points.p <- xyplot(y ~ x, data = loc.df, col = "black", 
-                     fill = clrs.hcl(length(eot.obj[[1]])), pch = 21,
+                     fill = clrs, pch = 21,
                      cex = 2)
   
   out <- pred.p + as.layer(points.p)
   
+  n <- length(eot.obj[[1]])
+  
+  grid.newpage()
+  
   map.vp <- viewport(x = 0, y = 0, 
-                     height = 1, width = 0.8,
-                     just = c("right", "bottom"))
+                     height = 1, width = 0.85,
+                     just = c("left", "bottom"))
   
   pushViewport(map.vp)
   
-  print(out)
+  print(out, newpage = FALSE)
+    
+  downViewport(trellis.vpname(name = "figure"))
   
-#   n <- length(eot.obj[[1]])
-#   
-#   if(n == 1) ypos <- 0.5 else ypos <- seq(0.95, 0.05, length.out = n + 2)
-#   if(n == 1) ypos <- ypos else ypos <- ypos[-c(1, length(ypos))]
-#   xpos.pts <- unit(0.1, "npc")
-#   size.pts <- 0.5 / n
-#   
-#   for (i in 1:n) {
-#     
-#     vp <- viewport(x = xpos.pts, y = ypos[i], 
-#                    height = size.pts, width = 0.1,
-#                    just = c("left", "centre"))
-#     
-#     pushViewport(vp)
-#     
-#   }
+  leg.vp <- viewport(x = 1, y = 0.5, 
+                     height = n / 10, width = 0.15,
+                     just = c("left", "centre"))
+  
+  pushViewport(leg.vp)  
+  
+  if(n == 1) ypos <- 0.5 else ypos <- seq(0.95, 0.05, length.out = n + 2)
+  if(n == 1) ypos <- ypos else ypos <- ypos[-c(1, length(ypos))]
+  xpos.pts <- unit(0.15, "npc")
+  size.pts <- 1 / n
+  
+  for (i in 1:n) {
+    
+    vp <- viewport(x = xpos.pts, y = ypos[i], 
+                   height = size.pts, width = 0.1,
+                   just = c("left", "centre"))
+    
+    pushViewport(vp)
+    
+    grid.circle(gp = gpar(fill = clrs[i], 
+                          col = "black"))
+    
+    upViewport()
+    
+  }
+  
+    xpos.txt <- unit(0.25, "npc")
+    width.txt <- 0.7
+    
+    for (i in 1:n) {
+      
+      vp <- viewport(x = xpos.txt, y = ypos[i], 
+                     height = size.pts, width = width.txt,
+                     just = c("left", "centre"))
+      
+      pushViewport(vp)
+      
+      txt <- resizingTextGrob(x = 0.2, sort(names(eot.obj[[1]]))[i],
+                              just = "left")
+      
+      grid.draw(txt)
+      
+      popViewport()
+      
+    }
+  
+  upViewport(0)
+  
     
   #return(eot.map)
  
