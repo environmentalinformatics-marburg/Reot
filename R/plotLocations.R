73a3a3aa5083da7c0fafe74dee0f906b872dd12b
tim-salabim <tim.appelhans@gmail.com>
2013-08-29 15:11:45 +0200

    BUGFIX: plotLocations now finds text grob function

--- R/plotLocations.R
+++ R/plotLocations.R
@@ -1,5 +1,35 @@
 plotLocations <- function(eot.obj) {
 
+  ### some helper functions especially for the grid layout
+  ### resizingTextGrob from 
+  ### http://ryouready.wordpress.com/2012/08/01/
+  resizingTextGrob <- function(..., scale.fact = 1) {
+    
+    grob(tg = textGrob(...), cl = "resizingTextGrob", 
+         scale.fact = scale.fact)
+    
+  }
+  
+  drawDetails.resizingTextGrob <- function(x, scale.fact, recording = TRUE) {
+    
+    grid.draw(x$tg)
+    
+  }
+  
+  preDrawDetails.resizingTextGrob <- function(x, ...) {
+    
+    library(scales)
+    
+    h <- convertHeight(unit(1, "npc"), "mm", valueOnly = TRUE)
+    fs <- rescale(h, to = c(80, 15), from = c(120, 20)) * x$scale.fact
+    pushViewport(viewport(gp = gpar(fontsize = fs)))
+    
+  }
+  
+  postDrawDetails.resizingTextGrob <- function(x) popViewport()
+  
+  
+  ### plot function
   loc.df <- as.data.frame(do.call("rbind", 
                                   lapply(seq(eot.obj[[1]]), function(i) {
     xyFromCell(eot.obj[[1]][[i]]$rsq.predictor, 
