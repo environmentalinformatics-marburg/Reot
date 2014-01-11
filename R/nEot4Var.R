2249d405b3e665874083969c8e2d922b85e908e1
tim-salabim <tim.appelhans@gmail.com>
2014-01-11 10:59:25 +0100

    UPDATE: adjusted nEot4Var to account for cumulative variances returned by eot now

--- R/nEot4Var.R
+++ R/nEot4Var.R
@@ -2,12 +2,11 @@
 #' variance within the response series.
 #' 
 #' @export nEot4Var
-nEot4Var <- function(eot.list, var = 1) {
+nEot4Var <- function(eot.list, var = 0.9) {
   expl.var <- sapply(seq(eot.list[[1]]), function(i) {
     eot.list[[1]][[i]]$exp.var
   })
-  
-  result <- Reduce("+", expl.var, accumulate = TRUE)
-  max(which(var - result >= 0), na.rm = TRUE)
+
+  max(which(var - expl.var >= 0), na.rm = TRUE)
   
 }
